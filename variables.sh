19bf4820f87b9e2be0dfd1927b5ecf19cc73681f
Niel Bornstein <niel@bornstein.atlanta.ga.us>
2018-04-02 14:25:46 -0400

    Added empty variables.sh file

---
+++ variables.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+# Resource group for the cluster
+export RESOURCE_GROUP= 
+
+# Availability set for the cluster
+export AVAILABILITY_SET=
+
+# Username to log in to the nodes
+export USERNAME=
+
+# Password to log in to the nodes
+export PASSWORD=
+
+# TODO: Use SSH key to log in
+
+# Network security group to use for all nodes
+export NSG=
+
+# Location for the nodes
+export LOCATION=
+
+# Name of VNet
+export VNET=
+
+# Name of Subnet
+export SUBNET=
