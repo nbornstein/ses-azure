0a072cb4719aaa0c93d37a83a3bdbeb16f371dc9
Niel M. Bornstein <nbornstein@suse.com>
2020-06-10 10:08:14 -0400

    Only startup in the resource group

--- startup_vms.sh
+++ startup_vms.sh
@@ -5,7 +5,7 @@
 
 # NOTE: Starts all VMs in the resource group!
 
-for NODENAME in $( az vm list -o table | tail -n +3 | cut -f 1 -d ' ' )
+for NODENAME in $( az vm list -o table --resource-group $RESOURCE_GROUP | tail -n +3 | cut -f 1 -d ' ' )
 do
 	echo "Starting $NODENAME"
 	az vm start --name $NODENAME --resource-group $RESOURCE_GROUP --no-wait
