d30f4c8f5c03851aaae075d26fad61184fb46178
Niel M. Bornstein <nbornstein@suse.com>
2020-06-11 22:30:39 -0400

    Variable numbers of OSD and test nodes and disks

--- tfm/main.tf
+++ tfm/main.tf
@@ -31,36 +31,23 @@ resource "azurerm_public_ip" "publicip-admin" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  domain_name_label   = "${var.prefix}-admin"
 }
-resource "azurerm_public_ip" "publicip-osd1" {
-  name                = "${var.prefix}-osd1-ip"
+resource "azurerm_public_ip" "publicip-osd" {
+  count               = var.num_osd
+  name                = "${var.prefix}-osd-${count.index}-ip"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-}
-resource "azurerm_public_ip" "publicip-osd2" {
-  name                = "${var.prefix}-osd2-ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
-resource "azurerm_public_ip" "publicip-osd3" {
-  name                = "${var.prefix}-osd3-ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
-resource "azurerm_public_ip" "publicip-osd4" {
-  name                = "${var.prefix}-osd4-ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+  domain_name_label   = "${var.prefix}-osd-${count.index}"
 }
 resource "azurerm_public_ip" "publicip-test" {
-  name                = "${var.prefix}-test-ip"
+  count               = var.num_test
+  name                = "${var.prefix}-test-${count.index}-ip"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  domain_name_label   = "${var.prefix}-test-${count.index}"
 }
 
 # Create Network Security Group and rule
@@ -118,69 +105,32 @@ resource "azurerm_network_interface" "nic-admin" {
     public_ip_address_id          = azurerm_public_ip.publicip-admin.id
   }
 }
-resource "azurerm_network_interface" "nic-osd1" {
-  name                      = "${var.prefix}-osd1-nic"
+resource "azurerm_network_interface" "nic-osd" {
+  count                     = var.num_osd
+  name                      = "${var.prefix}-osd-${count.index}-nic"
   location                  = var.location
   resource_group_name       = azurerm_resource_group.rg.name
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
-    name                          = "${var.prefix}-osd1-ip"
+    name                          = "${var.prefix}-osd-${count.index}-ip"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip-osd1.id
-  }
-}
-resource "azurerm_network_interface" "nic-osd2" {
-  name                      = "${var.prefix}-osd2-nic"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  network_security_group_id = azurerm_network_security_group.nsg.id
-
-  ip_configuration {
-    name                          = "${var.prefix}-osd2-ip"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip-osd2.id
-  }
-}
-resource "azurerm_network_interface" "nic-osd3" {
-  name                      = "${var.prefix}-osd3-nic"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  network_security_group_id = azurerm_network_security_group.nsg.id
-
-  ip_configuration {
-    name                          = "${var.prefix}-osd3-ip"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip-osd3.id
-  }
-}
-resource "azurerm_network_interface" "nic-osd4" {
-  name                      = "${var.prefix}-osd4-nic"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  network_security_group_id = azurerm_network_security_group.nsg.id
-
-  ip_configuration {
-    name                          = "${var.prefix}-osd4-ip"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip-osd4.id
+    public_ip_address_id          = azurerm_public_ip.publicip-osd[count.index].id
   }
 }
 resource "azurerm_network_interface" "nic-test" {
-  name                      = "${var.prefix}-test-nic"
+  count                     = var.num_test
+  name                      = "${var.prefix}-test-${count.index}-nic"
   location                  = var.location
   resource_group_name       = azurerm_resource_group.rg.name
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
-    name                          = "${var.prefix}-test-ip"
+    name                          = "${var.prefix}-test-${count.index}-ip"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip-test.id
+    public_ip_address_id          = azurerm_public_ip.publicip-test[count.index].id
   }
 }
 
@@ -228,22 +178,24 @@ resource "azurerm_virtual_machine" "vm-admin" {
       "sudo SUSEConnect -r ${var.sles_reg_code}",
       "sudo SUSEConnect -p ses/6/x86_64 -r ${var.ses_reg_code}",
       "sudo zypper ref && sudo zypper up -y && sudo zypper in -y salt-master",
+      "sudo echo \"master: ${var.prefix}-admin\" >> /etc/salt/minion",
       "sudo systemctl enable salt-master",
       "sudo reboot"
     ]
   }
 }
 
-# Create the test virtual machine
+# Create the test virtual machines
 resource "azurerm_virtual_machine" "vm-test" {
-  name                  = "${var.prefix}-test"
+  count                 = var.num_test
+  name                  = "${var.prefix}-test-${count.index}"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic-test.id]
+  network_interface_ids = [azurerm_network_interface.nic-test[count.index].id]
   vm_size               = var.test_vm_size
 
   storage_os_disk {
-    name              = "${var.prefix}-test-osdisk"
+    name              = "${var.prefix}-test-${count.index}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -257,7 +209,7 @@ resource "azurerm_virtual_machine" "vm-test" {
   }
 
   os_profile {
-    computer_name  = "${var.prefix}-test"
+    computer_name  = "${var.prefix}-test-${count.index}"
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
@@ -269,7 +221,7 @@ resource "azurerm_virtual_machine" "vm-test" {
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
-      host     = azurerm_public_ip.publicip-test.ip_address
+      host     = azurerm_public_ip.publicip-test[count.index].ip_address
       user     = var.admin_username
       password = var.admin_password
     }
@@ -280,4 +232,75 @@ resource "azurerm_virtual_machine" "vm-test" {
       "sudo reboot"
     ]
   }
+}
+
+# Create the OSD storage disks
+resource "azurerm_managed_disk" "disk-osd" {
+  count                = var.num_osd * var.num_disk
+  name                 = "${var.prefix}-osd-${floor(count.index / var.num_disk)}-disk-${floor(count.index % var.num_disk)}"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "4000"
+}
+
+# Create the OSD storage node virtual machines
+resource "azurerm_virtual_machine" "vm-osd" {
+  count                 = var.num_osd
+  name                  = "${var.prefix}-osd-${count.index}"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic-osd[count.index].id]
+  vm_size               = var.osd_vm_size
+
+  storage_os_disk {
+    name              = "${var.prefix}-osd-${count.index}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.image_version
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}-osd-${count.index}"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      host     = azurerm_public_ip.publicip-osd[count.index].ip_address
+      user     = var.admin_username
+      password = var.admin_password
+    }
+
+    inline = [
+      "sudo SUSEConnect -r ${var.sles_reg_code}",
+      "sudo zypper ref && sudo zypper up -y && sudo zypper in -y salt-minion",
+      "sudo echo \"master: ${var.prefix}-admin\" >> /etc/salt/minion",
+      "sudo systemctl enable salt-minion",
+      "sudo reboot"
+    ]
+  }
+}
+
+# Attach the OSD disk to the OSD VM
+resource "azurerm_virtual_machine_data_disk_attachment" "datadisk" {
+  count              = var.num_osd * var.num_disk
+  managed_disk_id    = azurerm_managed_disk.disk-osd[floor(count.index % var.num_disk)].id
+  virtual_machine_id = azurerm_virtual_machine.vm-osd[floor(count.index / var.num_disk)].id
+  lun                = floor(count.index % var.num_disk)
+  caching            = "ReadWrite"
 }
