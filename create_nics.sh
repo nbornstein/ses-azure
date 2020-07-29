f619d431902deb7705b0bba4916defbbae7e8a76
Niel M. Bornstein <nbornstein@suse.com>
2020-07-29 09:50:18 -0400

    Create network security group

--- create_nics.sh
+++ create_nics.sh
@@ -24,6 +24,40 @@ az network vnet subnet create \
 --resource-group $RESOURCE_GROUP \
 --vnet-name $VNET
 
+# CREATE NETWORK SECURITY GROUP
+
+echo "Creating Network Security Group for $PREFIX"
+az network nsg create \
+--resource-group $RESOURCE_GROUP \
+--name $PREFIX-nsg  
+
+az network nsg rule create \
+--nsg-name $PREFIX-nsg \
+--name $PREFIX-nsg-rule-ssh \
+--priority 100 \
+--access Allow \
+--description "SSH" \
+--destination-port-ranges 22 \
+--resource-group $RESOURCE_GROUP
+
+az network nsg rule create \
+--nsg-name $PREFIX-nsg \
+--name $PREFIX-nsg-rule-openattic \
+--priority 110 \
+--access Allow \
+--description "OpenAttic" \
+--destination-port-ranges 8443 \
+--resource-group $RESOURCE_GROUP
+
+az network nsg rule create \
+--nsg-name $PREFIX-nsg \
+--name $PREFIX-nsg-rul-grafana \
+--priority 120 \
+--access Allow \
+--description "Grafana" \
+--destination-port-ranges 3000 \
+--resource-group $RESOURCE_GROUP
+
 # CREATE ADMIN NODE
 
 NODENAME=$PREFIX-admin
@@ -42,7 +76,8 @@ az network nic create \
 -n $NODENAME-nic \
 --accelerated-networking true \
 --public-ip $NODENAME-public-ip \
--l $LOCATION
+-l $LOCATION \
+--network-security-group $PREFIX-nsg
 
 # CREATE TEST NODES
 
@@ -50,19 +85,12 @@ for NODE in $( seq 0 $(($NUM_TEST - 1)) )
 do
 	NODENAME=$PREFIX-test-$NODE
 
-    az network public-ip create \
-    --name $NODENAME-public-ip \
-    --resource-group $RESOURCE_GROUP \
-    --allocation-method Dynamic \
-    --dns-name $NODENAME-public-ip
-
     az network nic create \
     --resource-group $RESOURCE_GROUP \
     --vnet-name $VNET \
     --subnet $SUBNET \
     -n $NODENAME-nic \
     --accelerated-networking true \
-    --public-ip $NODENAME-public-ip \
     -l $LOCATION
 
 done
@@ -71,22 +99,16 @@ done
 
 for NODE in $( seq 0 $(($NUM_OSD - 1)) ) 
 do
-	NODENAME=$PREFIX-osd$NODE
+	NODENAME=$PREFIX-osd-$NODE
 
-    az network public-ip create \
-    --name $NODENAME-public-ip \
-    --resource-group $RESOURCE_GROUP \
-    --allocation-method Dynamic \
-    --dns-name $NODENAME-public-ip
-    
     az network nic create \
     --resource-group $RESOURCE_GROUP \
     --vnet-name $VNET \
     --subnet $SUBNET \
     -n $NODENAME-nic \
     --accelerated-networking true \
-    --public-ip $NODENAME-public-ip \
-    -l $LOCATION
+    -l $LOCATION \
+    --network-security-group $PREFIX-nsg
 
 done
 
